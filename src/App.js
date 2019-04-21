823535ba99d617ea7d50445ef07d67a8ea6544d7
santhosh <santhosh156@gmail.om>
2019-04-21 19:04:44 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        Phone Directory
+        {/* <div className="Header">
+          Phone Directory
+        </div>
+        <button>Add </button>
+        <div>
+          <span>Name </span><br />
+          <span>Phone </span>
+        </div> */}
+        <label htmlFor="name">Name: </label>
+        <input id="name" type="text" placeholder="Type Here" defaultValue="Santhosh"/>
       </div>
     );
   }
