f75d19c1855c5c8010ecfb2e27f0805a9559fdeb
santhosh <santhosh156@gmail.om>
2019-05-01 17:19:10 +0530

    Rendered Subscribers Dynamically Added ‘Delete’ button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,48 @@ import './App.css'
 
 class App extends Component {
   render() {
+
+    let subscribers = [
+      {
+        id: 1,
+        name: "Santhosh",
+        phone: "9597246630"
+      },
+      {
+        id: 2,
+        name: "Sowndarya",
+        phone: "9944709688"
+      },
+      {
+        id: 3,
+        name: "Hasini",
+        phone: "NA"
+      }
+    ];
+
     return (
       <div>
-        <Header/>
-        <button className="addButton">ADD</button>
-        <div className="details">
-          <span className="name">NAME </span><span className="phone">PHONE </span>
+        <Header />
+        <div class="component-body-container">
+          <button className="custom-btn add-btn">ADD</button>
+
+          <div className="grid-container heading-container">
+            <span className="name-heading">NAME </span>
+            <span className="phone-heading">PHONE </span>
+          </div>
+
+          {
+            subscribers.map(sub => {
+              return <div id={sub.id} className="grid-container">
+                  <span className="grid-item">{sub.name}</span>
+                  <span className="grid-item">{sub.phone}</span>
+                  <button className="custom-btn del-btn">DELETE</button>
+                </div>
+            })
+          }
+
         </div>
+
       </div>
     );
   }
