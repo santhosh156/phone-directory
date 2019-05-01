87ea33c8d4e6ae525270277981cb118fe1eefc81
santhosh <santhosh156@gmail.om>
2019-05-01 16:16:44 +0530

    Styled ‘Header’ (External Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Header.css'
 
 const Header = function() {
     return(
-        <div>
+        <div className="header">
           Phone Directory
         </div>
     );
